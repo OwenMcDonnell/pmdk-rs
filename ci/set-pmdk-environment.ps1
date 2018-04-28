75f6ccfe0589ad01c1d2bb19f73f4393bbf60457
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-04-27 23:25:04 -0700

    Update set-pmdk-environment.ps1

--- ci/set-pmdk-environment.ps1
+++ ci/set-pmdk-environment.ps1
@@ -4,11 +4,11 @@ $include_path = [Environment]::GetEnvironmentVariable("PMDK_IncludePath","User")
 Write-host $include_path
 if ($env:target.Contains("msvc")) {
      Write-host "${env:LIB};$library_path"
-     [Environment]::SetEnvironmentVariable("LIB", "$library_path", "User")
-     [Environment]::SetEnvironmentVariable("INCLUDE", "$include_path", "User")
+     [Environment]::SetEnvironmentVariable("LIB", "${env:LIB};$library_path", "Process")
+     [Environment]::SetEnvironmentVariable("INCLUDE", "${env:INCLUDE};$include_path", "Process")
 }
 if ($env:target.Contains("gnu")) {
      Write-host "in gnu"
-     [Environment]::SetEnvironmentVariable("LIBRARY_PATH", "${env:LIBRARY_PATH};$library_path", "User")
-     [Environment]::SetEnvironmentVariable("C_INCLUDE_PATH", "${env:C_INCLUDE_PATH};$include_path", "User")
+     [Environment]::SetEnvironmentVariable("LIBRARY_PATH", "${env:LIBRARY_PATH};$library_path", "Process")
+     [Environment]::SetEnvironmentVariable("C_INCLUDE_PATH", "${env:C_INCLUDE_PATH};$include_path", "Process")
 }
