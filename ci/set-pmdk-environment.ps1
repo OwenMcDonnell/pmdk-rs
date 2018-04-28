00f59fea7c7221f489bed1b9c7254debb9a01772
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-04-27 23:16:04 -0700

    Update set-pmdk-environment.ps1

--- ci/set-pmdk-environment.ps1
+++ ci/set-pmdk-environment.ps1
@@ -3,10 +3,12 @@ Write-host $library_path
 $include_path = [Environment]::GetEnvironmentVariable("PMDK_IncludePath","User")
 Write-host $include_path
 if ($env:target.Contains("msvc")) {
+     Write-host "in msvc"
      [Environment]::SetEnvironmentVariable("LIB", "${env:LIB};$library_path", "User")
      [Environment]::SetEnvironmentVariable("INCLUDE", "${env:INCLUDE};$include_path", "User")
 }
 if ($env:target.Contains("gnu")) {
+     Write-host "in gnu"
      [Environment]::SetEnvironmentVariable("LIBRARY_PATH", "${env:LIBRARY_PATH};$library_path", "User")
      [Environment]::SetEnvironmentVariable("C_INCLUDE_PATH", "${env:C_INCLUDE_PATH};$include_path", "User")
 }
